792d3c96738ce998facc87482afa2cad704dc86b
venkatesh <venkatesh9ui@gmail.com>
2021-10-28 22:07:11 -0400

    Solution: Using Ployfills + Solution Transpiling Code

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,25 @@ module.exports = {
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'assets', 'scripts'),
-    publicPath: 'assets/scripts/'
+    publicPath: 'assets/scripts/',
   },
   devtool: 'cheap-module-eval-source-map',
   // devServer: {
   //   contentBase: './'
   // }
-  plugins: [
-    new CleanPlugin.CleanWebpackPlugin()
-  ]
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
